bd3c07ce049b45d660c6cb650b0ea5a6eadbc1aa
Atila Guler <atilagulerdev@gmail.com>
2024-02-02 01:53:12 +0300

    create models

--- auctions/models.py
+++ auctions/models.py
@@ -3,4 +3,54 @@ from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    watchlist = models.ManyToManyField('Product', blank=True, related_name="watchers")
+
+    def __str__(self):
+        return f"{self.username}"
+
+    
+class Product(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name="products")
+    image = models.ImageField(upload_to='images/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name="products")
+    
+
+
+
+    def __str__(self):
+        return f"{self.name} - ${self.price}"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Bid(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name="bids")
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name="bids")
+    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.product} - ${self.bid}"
+    
+    
+
+
+# auction
+# bids
+# comments
