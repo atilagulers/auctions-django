0a3774043b3ddb6c80f5dea10ea7f405ffc00b16
Atila Guler <atilagulerdev@gmail.com>
2024-02-02 16:11:22 +0300

    add comment model

--- auctions/models.py
+++ auctions/models.py
@@ -42,3 +42,7 @@ class Bid(models.Model):
     def __str__(self):
         return f"{self.user.full_name} - {self.auction.title} - ${self.amount}"
  
+
+class Comment(models.Model):
+    message = models.CharField(max_length=256)
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name='user_comments')
