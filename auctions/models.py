011d3f4405e002e07c93d749083ca989e11f376e
Atila Guler <atilagulerdev@gmail.com>
2024-02-02 15:53:31 +0300

    update models

--- auctions/models.py
+++ auctions/models.py
@@ -3,54 +3,42 @@ from django.db import models
 
 
 class User(AbstractUser):
-    watchlist = models.ManyToManyField('Product', blank=True, related_name="watchers")
+    watchlist = models.ManyToManyField('Auction', blank=True, related_name="watch_list")
 
     def __str__(self):
         return f"{self.username}"
-
     
-class Product(models.Model):
-    name = models.CharField(max_length=64)
-    description = models.CharField(max_length=256)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name="products")
-    image = models.ImageField(upload_to='images/')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name="products")
-    
-
-
-
-    def __str__(self):
-        return f"{self.name} - ${self.price}"
-
-
 class Category(models.Model):
-    name = models.CharField(max_length=64)
+    title = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.title}"
+
+
+class Auction(models.Model):
+    title = models.CharField(max_length=64)
     description = models.CharField(max_length=256)
+    imageUrl = models.CharField(max_length=1000)
+    highest_bid = models.ForeignKey("Bid", on_delete=models.DO_NOTHING, related_name="bid_price")
+    user = models.ForeignKey("User", on_delete=models.DO_NOTHING, related_name='user_actions')
+    category = models.ForeignKey("Category", on_delete=models.DO_NOTHING, related_name='category_auctions')
+    isActive = models.BooleanField(default=True)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self) -> str:
+        return f"{self.title} - ${self.highest_bid.amount} - ({self.category})"
 
-    def __str__(self):
-        return f"{self.name}"
+
 
 class Bid(models.Model):
-    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name="bids")
-    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name="bids")
-    bid = models.DecimalField(max_digits=10, decimal_places=2)
+    auction = models.ForeignKey('Auction', on_delete=models.CASCADE, related_name="auction_bids")
+    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="user_bids")
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-
     def __str__(self):
-        return f"{self.product} - ${self.bid}"
-    
-    
-
-
-# auction
-# bids
-# comments
+        return f"{self.user.full_name} - {self.auction.title} - ${self.amount}"
+ 
